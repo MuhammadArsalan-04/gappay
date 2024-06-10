4bd0621b31d9bac465ce12fa64c26ab6bda5f35a
MuhammadArsalan04 <muhammadarsalan0404@gmail.com>
2024-06-10 23:18:23 +0500

    added configs file

---
+++ lib/configs/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const kPrimaryColor = Color(0xff2344CD);
+  static const kTextFieldFillColor = Color(0xffC4C9E2);
+  static const kMessageColor = Color(0xff68DB82);
+}
---
+++ lib/configs/res.dart
@@ -0,0 +1,5 @@
+class Res {
+  static const String kSplashLogo = 'assets/splash_logo.png';
+  static const String kAppbarLogo = 'assets/onboarding.png';
+  static const String kOnboarding = 'assets/logo.png';
+}
